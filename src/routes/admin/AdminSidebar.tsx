57fcf7cfd092bf7c16a0bcae4c89b273dfb7c61b
maffe <manfred.malmros@gmail.com>
2025-01-19 13:09:00 +0000

    Added localization

--- src/routes/admin/AdminSidebar.tsx
+++ src/routes/admin/AdminSidebar.tsx
@@ -1,91 +1,93 @@
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
 } from "@radix-ui/react-collapsible";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 const nollning = [
-  {
-    title: "Pirat",
-    year: 2018,
-  },
-  {
-    title: "Häst",
-    year: 2015,
-  },
+	{
+		title: "Pirat",
+		year: 2018,
+	},
+	{
+		title: "Häst",
+		year: 2015,
+	},
 ];
 
 const allmänt = [
-  {
-    title: "Nyheter",
-    url: "news",
-  },
-  {
-    title: "Evenemang",
-    url: "events",
-  },
+	{
+		title: "admin:news.self",
+		url: "news",
+	},
+	{
+		title: "admin:events.self",
+		url: "events",
+	},
 ];
 
 export function AdminSidebar() {
-  return (
-    <Sidebar>
-      <SidebarHeader>Adminpage</SidebarHeader>
-      <SidebarContent>
-        <Collapsible>
-          <SidebarGroup>
-            <SidebarGroupLabel>
-              <CollapsibleTrigger>Nollning</CollapsibleTrigger>
-            </SidebarGroupLabel>
-            <CollapsibleContent>
-              <SidebarGroupContent>
-                <SidebarMenu>
-                  {nollning.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                      <SidebarMenuButton asChild>
-                        <span>{item.title}</span>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  ))}
-                </SidebarMenu>
-              </SidebarGroupContent>
-            </CollapsibleContent>
-          </SidebarGroup>
-        </Collapsible>
-        <Collapsible>
-          <SidebarGroup>
-            <SidebarGroupLabel>
-              <CollapsibleTrigger>Allmänt</CollapsibleTrigger>
-            </SidebarGroupLabel>
-            <CollapsibleContent>
-              <SidebarGroupContent>
-                <SidebarMenu>
-                  {allmänt.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                      <SidebarMenuButton asChild>
-                        <Link to={item.url}>
-                          <span>{item.title}</span>
-                        </Link>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  ))}
-                </SidebarMenu>
-              </SidebarGroupContent>
-            </CollapsibleContent>
-          </SidebarGroup>
-        </Collapsible>
-      </SidebarContent>
-    </Sidebar>
-  );
+	const { t } = useTranslation();
+	return (
+		<Sidebar>
+			<SidebarHeader>{t("admin:title")}</SidebarHeader>
+			<SidebarContent>
+				<Collapsible>
+					<SidebarGroup>
+						<SidebarGroupLabel>
+							<CollapsibleTrigger>Nollning</CollapsibleTrigger>
+						</SidebarGroupLabel>
+						<CollapsibleContent>
+							<SidebarGroupContent>
+								<SidebarMenu>
+									{nollning.map((item) => (
+										<SidebarMenuItem key={item.title}>
+											<SidebarMenuButton asChild>
+												<span>{item.title}</span>
+											</SidebarMenuButton>
+										</SidebarMenuItem>
+									))}
+								</SidebarMenu>
+							</SidebarGroupContent>
+						</CollapsibleContent>
+					</SidebarGroup>
+				</Collapsible>
+				<Collapsible>
+					<SidebarGroup>
+						<SidebarGroupLabel>
+							<CollapsibleTrigger>Allmänt</CollapsibleTrigger>
+						</SidebarGroupLabel>
+						<CollapsibleContent>
+							<SidebarGroupContent>
+								<SidebarMenu>
+									{allmänt.map((item) => (
+										<SidebarMenuItem key={item.title}>
+											<SidebarMenuButton asChild>
+												<Link to={item.url}>
+													<span>{t(item.title)}</span>
+												</Link>
+											</SidebarMenuButton>
+										</SidebarMenuItem>
+									))}
+								</SidebarMenu>
+							</SidebarGroupContent>
+						</CollapsibleContent>
+					</SidebarGroup>
+				</Collapsible>
+			</SidebarContent>
+		</Sidebar>
+	);
 }
