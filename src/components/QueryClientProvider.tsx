3ebee17147c65088bc2bc1c41aac10c56e439baf
Benjamin Halasz <69871144+Equasa@users.noreply.github.com>
2025-02-09 21:06:11 +0100

    fixed client so that requests can be made (#29)

--- src/components/QueryClientProvider.tsx
+++ src/components/QueryClientProvider.tsx
@@ -5,11 +5,24 @@ import {
 	QueryClientProvider as ReactQueryClientProvider,
 } from "@tanstack/react-query";
 import type { PropsWithChildren } from "react";
-import { client } from "@/api";
+import { AuthService, client } from "@/api";
 
 client.setConfig({ baseUrl: "http://localhost:8000" });
 
 const queryClient = new QueryClient();
+
+const token = await AuthService.authJwtLogin({
+	body: { username: "boss@fsektionen.se", password: "dabdab" },
+});
+
+const myHeaders = new Headers();
+
+myHeaders.append(
+	"Authorization",
+	`${token.data?.token_type} ${token.data?.access_token}`,
+);
+
+client.setConfig({ headers: myHeaders });
 
 export default function QueryClientProvider({ children }: PropsWithChildren) {
 	return (
