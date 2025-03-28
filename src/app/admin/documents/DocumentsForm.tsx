039e1d8e95ea5cc9e1f3c6f71e52937904d4655f
Georg Elgeback <georgelgeback@gmail.com>
2025-03-28 14:41:33 +0000

    based commit

--- src/app/admin/documents/DocumentsForm.tsx
+++ src/app/admin/documents/DocumentsForm.tsx
@@ -14,115 +14,74 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { DialogTitle } from "@radix-ui/react-dialog";
 import { z } from "zod";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import {
-	createEventMutation,
-	getAllEventsQueryKey,
-} from "@/api/@tanstack/react-query.gen";
+// import {
+// 	createDocumentMutation,
+// 	getAllDocumentsQueryKey,
+// } from "@/api/@tanstack/react-query.gen";
 
 import { AdminChooseCouncil } from "@/widgets/AdminChooseCouncil";
-import { AdminChooseDates } from "@/widgets/AdminChooseDates";
 
-const eventsSchema = z.object({
-	title_sv: z.string().min(2),
-	title_en: z.string().min(2),
-	council_id: z.number().int(),
-	starts_at: z.string(),
-	ends_at: z.string(),
-	signup_start: z.string(),
-	signup_end: z.string(),
-	description_sv: z.string().max(1000),
-	description_en: z.string().max(1000),
-	max_event_users: z.number(),
-	priorities: z.array(z.string()),
-	all_day: z.boolean(),
-	signup_not_opened_yet: z.boolean(),
-	recurring: z.boolean(),
-	drink: z.boolean(),
-	food: z.boolean(),
-	cash: z.boolean(),
-	closed: z.boolean(),
-	can_signup: z.boolean(),
-	drink_package: z.boolean(),
+
+const documentsSchema = z.object({
+  title: z.string(),
+  // description: "",
+  file: z.instanceof(File),
+  public: z.boolean(),
+  uploader_id: z.string(),
+  upload_date: z.string(),
+  edit_date: z.string(),
 });
 
-export default function EventsForm() {
+export default function DocumentsForm() {
 	const [open, setOpen] = useState(false);
 	const [submitEnabled, setSubmitEnabled] = useState(true);
 
-	const eventsForm = useForm<z.infer<typeof eventsSchema>>({
-		resolver: zodResolver(eventsSchema),
+	const documentsForm = useForm<z.infer<typeof documentsSchema>>({
+		resolver: zodResolver(documentsSchema),
 		defaultValues: {
-			title_sv: "",
-			title_en: "",
-			council_id: 0,
-			starts_at: new Date().toISOString(),
-			ends_at: new Date().toISOString(),
-			signup_start: new Date().toISOString(),
-			signup_end: new Date().toISOString(),
-			description_sv: "",
-			description_en: "",
-			max_event_users: 0,
-			priorities: [],
-			all_day: false,
-			signup_not_opened_yet: false,
-			recurring: false,
-			drink: false,
-			food: false,
-			cash: false,
-			closed: false,
-			can_signup: false,
-			drink_package: false,
+			title: "",
+			// description: "",
+      file: new File(["This is an example file. You may treat getting this as an error."], "sample.txt"),
+      public: false,
 		},
 	});
 
 	const queryClient = useQueryClient();
 
-	const createEvents = useMutation({
-		...createEventMutation(),
-		onSuccess: () => {
-			queryClient.invalidateQueries({ queryKey: getAllEventsQueryKey() });
-			setOpen(false);
-			setSubmitEnabled(true);
-		},
-		onError: () => {
-			setOpen(false);
-			setSubmitEnabled(true);
-		},
-	});
+	// const createDocuments = useMutation({
+	// 	...createDocumentMutation(),
+	// 	onSuccess: () => {
+	// 		queryClient.invalidateQueries({ queryKey: getAllEventsQueryKey() });
+	// 		setOpen(false);
+	// 		setSubmitEnabled(true);
+	// 	},
+	// 	onError: () => {
+	// 		setOpen(false);
+	// 		setSubmitEnabled(true);
+	// 	},
+	// });
 
-	function onSubmit(values: z.infer<typeof eventsSchema>) {
+	function onSubmit(values: z.infer<typeof documentsSchema>) {
 		setSubmitEnabled(false);
-		createEvents.mutate({
-			body: {
-				title_sv: values.title_sv,
-				title_en: values.title_en,
-				council_id: values.council_id,
-				starts_at: new Date(values.starts_at),
-				ends_at: new Date(values.ends_at),
-				signup_start: new Date(values.signup_start),
-				signup_end: new Date(values.signup_end),
-				description_sv: values.description_sv,
-				description_en: values.description_en,
-				max_event_users: values.max_event_users,
-				priorities: [],
-				all_day: values.all_day,
-				signup_not_opened_yet: values.signup_not_opened_yet,
-				recurring: values.recurring,
-				drink: values.drink,
-				food: values.food,
-				cash: values.cash,
-				closed: values.closed,
-				can_signup: values.can_signup,
-				drink_package: values.drink_package,
-			},
-		});
+		// createDocuments.mutate({
+		// 	body: {
+		// 		title: values.title,
+    //     // description: values.description,
+    //     file: values.file,
+    //     public: values.public,
+    //     uploader_id: values.uploader_id,
+    //     upload_date: values.upload_date,
+    //     edit_date: values.edit_date,
+		// 	},
+		// });
+    
 	}
 
 	return (
 		<div className="p-3">
 			<Button
 				onClick={() => {
-					eventsForm.reset();
+					documentsForm.reset();
 					setOpen(true);
 					setSubmitEnabled(true);
 				}}
@@ -136,102 +95,33 @@ export default function EventsForm() {
 						<DialogTitle>Skapa event</DialogTitle>
 					</DialogHeader>
 					<hr />
-					<Form {...eventsForm}>
+					<Form {...documentsForm}>
 						<form
-							onSubmit={eventsForm.handleSubmit(onSubmit)}
+							onSubmit={documentsForm.handleSubmit(onSubmit)}
 							className="grid gap-x-4 gap-y-3 lg:grid-cols-4"
 						>
 							<FormField
-								control={eventsForm.control}
-								name="title_sv"
+								control={documentsForm.control}
+								name="title"
 								render={({ field }) => (
 									<FormItem>
-										<FormLabel>Titel (sv)</FormLabel>
+										<FormLabel>Titel</FormLabel>
 										<FormControl>
 											<Input placeholder="Titel" {...field} />
 										</FormControl>
 									</FormItem>
 								)}
 							/>
-							<FormField
-								control={eventsForm.control}
-								name="title_en"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Titel (en)</FormLabel>
-										<FormControl>
-											<Input placeholder="Title" {...field} />
-										</FormControl>
-									</FormItem>
-								)}
-							/>
 
 							<FormField
-								control={eventsForm.control}
-								name="council_id"
+								control={documentsForm.control}
+								name="file"
 								render={({ field }) => (
 									<FormItem className="lg:col-span-2">
-										<FormLabel>Council name</FormLabel>
-										<AdminChooseCouncil
-											value={field.value}
-											onChange={field.onChange}
-										/>
-									</FormItem>
-								)}
-							/>
-
-							<FormField
-								control={eventsForm.control}
-								name="starts_at"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Starttime</FormLabel>
-										<AdminChooseDates
-											value={field.value}
-											onChange={field.onChange}
-										/>
-									</FormItem>
-								)}
-							/>
-
-							<FormField
-								control={eventsForm.control}
-								name="ends_at"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Endtime</FormLabel>
-										<AdminChooseDates
-											value={field.value}
-											onChange={field.onChange}
-										/>
-									</FormItem>
-								)}
-							/>
-
-							<FormField
-								control={eventsForm.control}
-								name="signup_start"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Signup start</FormLabel>
-										<AdminChooseDates
-											value={field.value}
-											onChange={field.onChange}
-										/>
-									</FormItem>
-								)}
-							/>
-
-							<FormField
-								control={eventsForm.control}
-								name="signup_end"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Signup end</FormLabel>
-										<AdminChooseDates
-											value={field.value}
-											onChange={field.onChange}
-										/>
+										<FormLabel>Fil</FormLabel>
+										<FormControl>
+                      <Input id="file" type="file" {...field} /> 
+                    </FormControl>
 									</FormItem>
 								)}
 							/>
@@ -248,6 +138,7 @@ export default function EventsForm() {
 									Publicera
 								</Button>
 							</div>
+
 						</form>
 					</Form>
 				</DialogContent>
