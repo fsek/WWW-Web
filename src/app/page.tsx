c2b291bd2004e18841ff22fd81b30e4e202bc117
Georg Elgeback <georgelgeback@gmail.com>
2025-03-28 18:13:35 +0000

    Crazy edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
 import Link from "next/link";
-import { NavBar } from "../components/NavBar";
-import Footer from "@/components/Footer";
 
 export default function MainLanding() {
 	return (
-		<div className="flex flex-col min-h-screen">
-			<NavBar />
-			<div className="flex-grow">
-				<p>
-					Hej! 👋 Du 🫵 ser 👀 denna 📄 sida 📘 för 💡 att ✨ du 🫶 är 🧊 cool
-					😎 och 🕸️ spindel 🕷️.
-				</p>
-				<p>👉👈</p>
-				<Link href="admin/documents">Goto news</Link>
-			</div>
-			<Footer />
+		<div className="flex-grow">
+			<p>
+				Hej! 👋 Du 🫵 ser 👀 denna 📄 sida 📘 för 💡 att ✨ du 🫶 är 🧊 cool
+				😎 och 🕸️ spindel 🕷️.
+			</p>
+			<p>👉👈</p>
+			<Link href="admin/documents">Goto news</Link>
 		</div>
 	);
 }
