4cf2102d4db88e32c5bfa39a9b8244235eed6eed
maffe <manfred.malmros@gmail.com>
2025-04-02 15:45:38 +0000

    re-removed navbar whoops

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,12 @@ import Footer from "@/components/Footer";
 export default function MainLanding() {
 	return (
 		<div className="flex flex-col min-h-screen">
-			<NavBar />
 			<div className="flex-grow">
 				<p>
 					Hej! 👋 Du 🫵 ser 👀 denna 📄 sida 📘 för 💡 att ✨ du 🫶 är 🧊 cool
 					😎 och 🕸️ spindel 🕷️.
 				</p>
-        <p>👉👈</p>
+				<p>👉👈</p>
 				<Link href="admin/documents">Goto news</Link>
 			</div>
 			<div className="">
