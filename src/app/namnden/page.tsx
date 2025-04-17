e8dda45ec90e3b2d4d60395f00bf04f7e1ebc7be
Georg Elgeback <georgelgeback@gmail.com>
2025-04-17 12:48:24 +0000

    Added page to its own namespace

--- src/app/namnden/page.tsx
+++ src/app/namnden/page.tsx
@@ -15,7 +15,7 @@ export default function NamndenPage() {
 	return (
 		<div className="flex flex-col min-h-screen">
 			<TitleBanner
-				title={t("main:namnden.title")}
+				title={t("namnden:title")}
 				imageUrl={mh.src} // this errors in my typecheck but works somehow
 				className="relative h-[30vh] bg-cover bg-center"
 			/>
@@ -24,25 +24,27 @@ export default function NamndenPage() {
 					leftColumnContent={
 						<>
 							<CustomTitle
-								text={t("main:namnden.about_title")}
+								text={t("namnden:about_title")}
 								className="mt-4"
 								size={3}
 							/>
-							<p className="mt-4"> {/*Inline translated link example*/}
-								<Trans i18nKey="main:namnden.about"> 
+							<p className="mt-4">
+								{/*Inline translated link example*/}
+								<Trans i18nKey="namnden:about">
 									<a
 										href="/admin"
 										className="underline text-orange-500 hover:text-orange-400"
 										target="_blank"
 										rel="noreferrer"
 									>
-										This text here can be whatever, it will however show up if the translation is not found.
-										This is a fake link to the page of the vice president (replace with real link).
-										NOTE: You do need some text, even just a single character here, otherwise the translation will not work.
+										This text here can be whatever, it will however show up if
+										the translation is not found. This is a fake link to the
+										page of the vice president (replace with real link). NOTE:
+										You do need some text, even just a single character here,
+										otherwise the translation will not work.
 										{/* When writing the language file for a component like this, the elements within Trans are numbered
 										and you can write the text to be inputted to them <0>in xlm</0> tags, then 1, 2, 3 etc.
 										NOTE 2: Comments in curly brackets (like this one) count as elements. Yes, really. */}
-										
 									</a>
 								</Trans>
 							</p>
@@ -51,11 +53,11 @@ export default function NamndenPage() {
 					rightColumnContent={
 						<>
 							<CustomTitle
-								text={t("main:namnden.contact_title")}
+								text={t("namnden:contact_title")}
 								className="mt-4"
 								size={3}
 							/>
-							<p className="mt-4">{t("main:namnden.contact")}</p>
+							<p className="mt-4">{t("namnden:contact")}</p>
 						</>
 					}
 					className="p-14 gap-12"
