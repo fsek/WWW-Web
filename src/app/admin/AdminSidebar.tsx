8ff8d350267caa8fefbaf0a53a38b235ad79cd94
maffe <manfred.malmros@gmail.com>
2025-05-19 15:31:38 +0000

    Admin sidebar permissions added

--- src/app/admin/AdminSidebar.tsx
+++ src/app/admin/AdminSidebar.tsx
@@ -20,43 +20,75 @@ import {
 } from "@/components/ui/collapsible";
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
+import { usePermissions } from "@/lib/auth";
+import type { PermissionRead } from "@/api";
+import { useMemo } from "react";
 
-const groups = {
-	Allmänt: [
-		{
-			title: "admin:news.self",
-			url: "/admin/news",
-			icon: Newspaper,
-			permissions: [{ target: "News", action: "manage" }],
-		},
-		{
-			title: "admin:events.self",
-			url: "/admin/events",
-			icon: Calendar,
-			permissions: [{ target: "Event", action: "manage" }],
-		},
-		{
-			title: "admin:documents.self",
-			url: "/admin/documents",
-			icon: FileText,
-			permissions: [{ target: "Document", action: "manage" }],
-		},
-	],
-};
+interface SidebarItem {
+	title: string;
+	url: string;
+	icon: React.ComponentType;
+	permissions: [
+		{ target: PermissionRead["target"]; action: PermissionRead["action"] },
+	];
+}
+
+const groups: { title: string; content: SidebarItem[] }[] = [
+	{
+		title: "admin:general",
+		content: [
+			{
+				title: "admin:news.self",
+				url: "/admin/news",
+				icon: Newspaper,
+				permissions: [{ target: "News", action: "manage" }],
+			},
+			{
+				title: "admin:events.self",
+				url: "/admin/events",
+				icon: Calendar,
+				permissions: [{ target: "Event", action: "manage" }],
+			},
+			{
+				title: "admin:documents.self",
+				url: "/admin/documents",
+				icon: FileText,
+				permissions: [{ target: "Document", action: "manage" }],
+			},
+		],
+	},
+];
 
 export function AdminSidebar() {
 	const { t } = useTranslation();
-
+	const permissions = usePermissions();
+	const filteredGroups = useMemo(() => {
+		return groups.reduce(
+			(acc, group) => {
+				const filteredItems = group.content.filter((item) =>
+					item.permissions.every(
+						(permission) =>
+							permissions.get(permission.target) === permission.action,
+					),
+				);
+				if (filteredItems.length > 0) {
+					acc[group.title] = filteredItems;
+				}
+				return acc;
+			},
+			{} as Record<string, SidebarItem[]>,
+		);
+	}, [permissions]);
 	return (
 		<Sidebar>
 			<SidebarHeader>{t("admin:title")}</SidebarHeader>
 			<SidebarContent>
-				{Object.entries(groups).map(([group, items]) => (
+				{Object.entries(filteredGroups).map(([group, items]) => (
 					<Collapsible defaultOpen className="group/collapsible" key={group}>
 						<SidebarGroup>
 							<SidebarGroupLabel asChild>
 								<CollapsibleTrigger>
-									{group}
+									{t(group)}
 									<ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
 								</CollapsibleTrigger>
 							</SidebarGroupLabel>
@@ -64,16 +96,26 @@ export function AdminSidebar() {
 								<SidebarGroup>
 									<SidebarGroupContent>
 										<SidebarMenu>
-											{items.map((item) => (
-												<SidebarMenuItem key={item.title}>
-													<SidebarMenuButton asChild>
-														<Link href={item.url}>
-															{<item.icon />}
-															<span>{t(item.title)}</span>
-														</Link>
-													</SidebarMenuButton>
-												</SidebarMenuItem>
-											))}
+											{items.map((item) => {
+												if (
+													item.permissions.every(
+														(permission) =>
+															permissions.get(permission.target) ===
+															permission.action,
+													)
+												) {
+													return (
+														<SidebarMenuItem key={item.title}>
+															<SidebarMenuButton asChild>
+																<Link href={item.url}>
+																	{<item.icon />}
+																	<span>{t(item.title)}</span>
+																</Link>
+															</SidebarMenuButton>
+														</SidebarMenuItem>
+													);
+												}
+											})}
 										</SidebarMenu>
 									</SidebarGroupContent>
 								</SidebarGroup>
