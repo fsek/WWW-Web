24e8c5d862e3d818034e42078a1640d5469ac7f3
maffe <manfred.malmros@gmail.com>
2025-07-21 14:32:54 +0000

    use client missing hindering compile :p

--- src/app/(public)/documents/page.tsx
+++ src/app/(public)/documents/page.tsx
@@ -1,18 +1,16 @@
+"use client";
+
 import { useTranslation } from "react-i18next";
 
 export default function DocumentPage() {
-  const { t } = useTranslation();
+	const { t } = useTranslation();
 	return (
 		<div>
 			<h1 className="text-2xl font-bold">{t("main:documents.title")}</h1>
-      <p className="text-neutral-500">
-        {t("main:documents.title")}
-      </p>
-      <div className="flex flex-col gap-4">
-        <p>
-          Document 
-        </p>
-      </div>
+			<p className="text-neutral-500">{t("main:documents.title")}</p>
+			<div className="flex flex-col gap-4">
+				<p>Document</p>
+			</div>
 		</div>
 	);
 }
