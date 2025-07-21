5cc9733728744a162e2e3f17af7090e853237a02
maffe <manfred.malmros@gmail.com>
2025-07-21 14:24:10 +0000

    Make sure we only init once

--- src/app/i18n.tsx
+++ src/app/i18n.tsx
@@ -24,41 +24,43 @@ export default async function initTranslations(
 ) {
 	i18nInstance = i18nInstance || createInstance();
 
-	i18nInstance.use(initReactI18next);
+	if (!i18nInstance.isInitialized) {
+		i18nInstance.use(initReactI18next);
 
-	i18nInstance.use(LanguageDetector);
+		i18nInstance.use(LanguageDetector);
 
-	if (!resources) {
-		i18nInstance.use(
-			resourcesToBackend(
-				(locale: Locale, namespace: Namespace) =>
-					import(`@/locales/${locale}/${namespace}.json`),
-			),
-		);
+		if (!resources) {
+			i18nInstance.use(
+				resourcesToBackend(
+					(locale: Locale, namespace: Namespace) =>
+						import(`@/locales/${locale}/${namespace}.json`),
+				),
+			);
+		}
+
+		const detectionOptions = {
+			order: ["querystring", "cookie", "localStorage", "navigator", "htmlTag"],
+			caches: ["cookie", "localStorage"],
+			load: "languageOnly",
+			// Don't detect from path, subdomain, or querystring (no internationalized routing)
+			excludeCacheFor: ["cimode"],
+		};
+
+		await i18nInstance.init({
+			lng: undefined,
+			resources,
+			fallbackLng: i18nConfig.defaultLocale,
+			supportedLngs: i18nConfig.locales,
+			defaultNS: namespaces[0],
+			fallbackNS: namespaces[0],
+			ns: namespaces,
+			preload: resources ? [] : i18nConfig.locales,
+			react: {
+				useSuspense: false,
+			},
+			detection: detectionOptions,
+		});
 	}
-
-	const detectionOptions = {
-		order: ["querystring", "cookie", "localStorage", "navigator", "htmlTag"],
-		caches: ["cookie", "localStorage"],
-		load: "languageOnly",
-		// Don't detect from path, subdomain, or querystring (no internationalized routing)
-		excludeCacheFor: ["cimode"],
-	};
-
-	await i18nInstance.init({
-		lng: locale,
-		resources,
-		fallbackLng: i18nConfig.defaultLocale,
-		supportedLngs: i18nConfig.locales,
-		defaultNS: namespaces[0],
-		fallbackNS: namespaces[0],
-		ns: namespaces,
-		preload: resources ? [] : i18nConfig.locales,
-		react: {
-			useSuspense: false,
-		},
-		detection: detectionOptions,
-	});
 
 	return {
 		i18n: i18nInstance,
