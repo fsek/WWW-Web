fb50367a4c7ff2808fd9a7ce37036138323dc098
maffe03 <manfred.malmros@gmail.com>
2025-07-21 21:35:14 +0200

    New authentication methods (#80)
    * Modified auth to work with refresh tokens
    
    * Applied suggestions from review
    
    * Add margin for transit time
    
    * Added member administration page (#78)
    
    * Added member administration page
    
    * Update to use proper bulk PATCH router
    
    * Fix indentation
    
    ---------
    
    Co-authored-by: Georg <50551748+georgelgeback@users.noreply.github.com>

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,25 +1,42 @@
 "use client";
 
 import type { BearerResponse } from "@/api";
-import { useEffect, useState } from "react";
+import { create } from "zustand";
 
-export function handleBearerResponse(data: BearerResponse) {
-	localStorage.setItem(
-		"authorization",
-		`${data.token_type} ${data.access_token}`,
-	);
-}
+type AuthState = {
+	accessToken: BearerResponse | null;
+	setAccessToken: (data: BearerResponse) => void;
+	authorizationHeader: () => string | null;
+	isAuthenticated: () => boolean;
+};
 
-export function getAuthorizationHeader() {
-	return localStorage.getItem("authorization");
-}
+export const useAuthState = create<AuthState>((set, get) => ({
+	accessToken: null,
+	setAccessToken: (data: BearerResponse) => {
+		set({ accessToken: data });
+	},
+	authorizationHeader() {
+		const accessToken = get().accessToken;
+		if (accessToken) {
+			// We convert exp to milliseconds manually to be more explicit
+			// Date.now() is in milliseconds, exp is in seconds
+			const payload = JSON.parse(
+				Buffer.from(
+					accessToken.access_token.split(".")[1],
+					"base64",
+				).toString(),
+			);
+			const expiryMilliseconds = payload.exp * 1000 - (2 * 1000); // Two second margin
 
-export function useAuthState() {
-	const [isAuthenticated, setIsAuthenticated] = useState<boolean>();
-
-	useEffect(() => {
-		setIsAuthenticated(!!getAuthorizationHeader());
-	}, []);
-
-	return isAuthenticated;
-}
+			if (expiryMilliseconds < Date.now()) {
+				// The token has expired. Return null.
+				return null;
+			}
+			return `${accessToken.token_type} ${accessToken.access_token}`;
+		}
+		return null; // No token available
+	},
+	isAuthenticated() {
+		return !!get().authorizationHeader();
+	},
+}));
