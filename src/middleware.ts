09209fccb1c5bf3364acda9f3722cfff0aff1d5f
Georg Elgeback <georgelgeback@gmail.com>
2025-07-26 21:35:10 +0000

    Send unath users to index from /home

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { NextResponse, type NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
 	const authStatus = request.cookies.get("auth_status");
 
+	// Redirect authenticated users from / to /home
 	if (
 		authStatus?.value === "authenticated" &&
 		request.nextUrl.pathname === "/"
@@ -10,9 +11,17 @@ export function middleware(request: NextRequest) {
 		return NextResponse.redirect(new URL("/home", request.url));
 	}
 
+	// Redirect unauthenticated users from /home to /
+	if (
+		authStatus?.value !== "authenticated" &&
+		request.nextUrl.pathname === "/home"
+	) {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
+
 	return NextResponse.next();
 }
 
 export const config = {
-	matcher: ["/"],
+	matcher: ["/", "/home"],
 };
