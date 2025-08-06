7b63c7e88c12dd5a680ec051d7dc99083df7dabd
Olle Settergren <olle.settergren@gmail.com>
2025-08-06 10:36:34 +0000

    Fixed spacing, better looping and reset on language switch

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -5,8 +5,23 @@ import TypewriterClient from "typewriter-effect";
 import bannerImg from "@/assets/landing/banner.jpg";
 import Image from "next/image";
 
+import { useState, useEffect } from "react";
+
 export const Hero = () => {
-	const { t } = useTranslation("landingpage");
+	const { t, i18n } = useTranslation("landingpage");
+	const [reset, setReset] = useState(false);
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		// This effect will run whenever the language changes
+		setReset(true);
+		const timer = setTimeout(() => {
+			setReset(false);
+		}, 10); // Small delay to ensure the component resets
+
+		return () => clearTimeout(timer);
+	}, [i18n.language]);
+
 	return (
 		<section className="relative items-center place-items-center py-20 md:py-60 md:h-screen gap-10 w-full flex flex-col">
 			{/* Fullwidth background image (Is LCP) */}
@@ -31,35 +46,43 @@ export const Hero = () => {
 			<div className="container relative z-20 text-start space-y-6 px-10 md:px-0">
 				<div className="backdrop-blur-xs bg-white/10 border border-white/20 rounded-2xl p-8 md:p-12 shadow-xl space-y-8 w-full lg:w-6/10">
 					<div className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold flex flex-wrap items-start gap-2 flex-col justify-start">
-						<div className="inline text-primary">{t("hero.title_static")}</div>
-						<TypewriterClient
-							options={{
-								loop: true,
-								delay: 75,
-								wrapperClassName:
-									"text-primary justify-start whitespace-nowrap",
-								cursorClassName: "text-white animate-blink",
-								cursor: "|",
-							}}
-							onInit={(typewriter) => {
-								typewriter
-									.typeString(t("hero.title_dynamic_start"))
-									.typeString(t("hero.title_dynamic_1"))
-									.pauseFor(2500)
-									.deleteChars(t("hero.title_dynamic_1").length)
-									.typeString(
-										`<span style='color: #bebebe;'>${t("hero.title_dynamic_2")}</span>`,
-									)
-									.pauseFor(2500)
-									.deleteChars(t("hero.title_dynamic_2").length)
-									.typeString(
-										`<span style='color: #4e9d2d;'>${t("hero.title_dynamic_3")}</span>`,
-									)
-									.pauseFor(2500)
-									.deleteAll()
-									.start();
-							}}
-						/>
+						<div className="inline text-primary">
+							{t("hero.title_static")}
+							{}
+						</div>
+						<div className="text-primary gap-3 justify-start whitespace-nowrap flex flex-row">
+							{t("hero.title_dynamic_start")}
+							{}
+							{!reset && (
+								<TypewriterClient
+									options={{
+										loop: true,
+										delay: 75,
+										wrapperClassName:
+											"inline text-primary justify-start whitespace-nowrap",
+										cursorClassName: "text-white animate-blink",
+										cursor: "|",
+									}}
+									onInit={(typewriter) => {
+										typewriter
+											.typeString(t("hero.title_dynamic_1"))
+											.pauseFor(2500)
+											.deleteAll()
+											.typeString(
+												`<span style='color: #bebebe;'>${t("hero.title_dynamic_2")}</span>`,
+											)
+											.pauseFor(2500)
+											.deleteAll()
+											.typeString(
+												`<span style='color: #4e9d2d;'>${t("hero.title_dynamic_3")}</span>`,
+											)
+											.pauseFor(2500)
+											.deleteAll()
+											.start();
+									}}
+								/>
+							)}
+						</div>
 					</div>
 
 					<p className="md:text-xl text-white md:w-10/12 mx-auto lg:mx-0">
