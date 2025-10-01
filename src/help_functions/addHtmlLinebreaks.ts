304efd43267eaabc295544081cffad2f1861f869
Georg <50551748+georgelgeback@users.noreply.github.com>
2025-10-01 16:18:07 +0200

    Reverted an old fix which is no longer needed. (#187)

--- src/help_functions/addHtmlLinebreaks.ts
+++
@@ -1,9 +0,0 @@
-// Helper to add HTML linebreaks to descriptions
-// No longer used, this was a temporary fix
-export default function addHtmlLinebreaks(
-	value: string | null | undefined,
-): string {
-	if (!value) return "";
-	// Add html newlines where they should be
-	return value.replace(/\n/g, "<br />\n");
-}
