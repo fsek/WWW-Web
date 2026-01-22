cc1fa61c557330fbc12dc7eff1f6e46832d1b147
Manfred Malmros <manfred.malmros@gmail.com>
2026-01-22 20:26:35 +0000

    typescript is a fake language. whatever... documents are done now

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -13,14 +13,17 @@ import { Button } from "@/components/ui/button";
 import { NavBar } from "@/components/NavBar";
 import { Footer } from "@/components/Footer";
 import { Info } from "lucide-react";
+import InfoBox from "@/widgets/InfoBox";
 
 export default function Documents() {
 	// TODO: Fix this page lmao
-	const { t } = useTranslation();
+	const { t } = useTranslation("main");
 	const { data, error, isPending } = useQuery({
 		...getAllDocumentsOptions(),
 	});
 	const [searchTitle, setSearchTitle] = useState<string>("");
+	const [selectedDocument, setSelectedDocument] = useState<DocumentRead>();
+	const [infoBoxOpen, setInfoBoxOpen] = useState<boolean>(false);
 
 	// Column setup
 	const columnHelper = createColumnHelper<DocumentRead>();
@@ -62,8 +65,8 @@ export default function Documents() {
 						variant="outline"
 						onClick={(e) => {
 							e.stopPropagation();
-							const url = `/documents/${row.row.original.id}`;
-							window.open(url, "_blank", "noopener,noreferrer");
+							setSelectedDocument(row.row.original);
+							setInfoBoxOpen(true);
 						}}
 					>
 						<Info />
@@ -96,6 +99,41 @@ export default function Documents() {
 		<div className="flex flex-col min-h-screen">
 			<NavBar />
 			<main className="flex-1">
+				{selectedDocument && (
+					<InfoBox
+						open={infoBoxOpen}
+						onClose={() => setInfoBoxOpen(false)}
+						displayData={[
+							// Look i really did try to make this better but typescript doesn't have a type system :)
+							{ label: t("documents.title"), value: selectedDocument.title },
+							{
+								label: t("documents.author"),
+								value: `${selectedDocument.author.first_name} ${selectedDocument.author.last_name}`,
+							},
+							{
+								label: t("documents.file_name"),
+								value: selectedDocument.file_name,
+							},
+							{
+								label: t("documents.category"),
+								value: selectedDocument.category,
+							},
+							{
+								label: t("documents.created_at"),
+								value: selectedDocument.created_at.toLocaleString(),
+							},
+							{
+								label: t("documents.updated_at"),
+								value: selectedDocument.updated_at.toLocaleString(),
+							},
+							{
+								label: t("documents.is_private"),
+								value: selectedDocument.is_private ? t("yes") : t("no"),
+							},
+						]}
+						lg_columns={3}
+					/>
+				)}
 				<div className="px-8 space-x-4 w-full lg:w-[80%] mx-auto">
 					<h3 className="text-3xl py-3 font-bold text-primary">
 						{t("main:documents.page_title")}
