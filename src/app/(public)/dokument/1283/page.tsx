4c66b322fd954b97c584b0ba1457e68aa8c5a22a
Georg Elgebäck <50551748+georgelgeback@users.noreply.github.com>
2026-04-23 14:43:42 +0200

    Remove or replace links pointing to the old page and update/remove some text. (#291)

--- src/app/(public)/dokument/1283/page.tsx
+++ src/app/(public)/dokument/1283/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 export default function Document() {
 	// Redirect to the Terms and conditions for rental of the Facilities Committee’s equipment (temporary link)
 	// This is because the terms contains a link to the old site I cannot change
-	redirect("https://old.fsektionen.se/dokument/1283");
+	redirect("https://fsektionen.se/documents/32");
 }
