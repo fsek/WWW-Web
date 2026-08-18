e2431acf5f125d85d5e2b93196f8f2eb4229de2b
Georg Elgebäck <50551748+georgelgeback@users.noreply.github.com>
2026-08-18 14:39:51 +0200

    Add category field to mission admin pages (#324)
    * Add category field to mission admin pages
    
    * Localized categories
    
    * Lägsta/högsta instead of min/max
    
    ---------
    
    Co-authored-by: Hannes Blåman <hannes@blaman.com>

--- src/constants.ts
+++ src/constants.ts
@@ -20,3 +20,16 @@ export type MentorGroupType =
 export const API_BASE_URL =
 	process.env.NEXT_PUBLIC_API_BASE_URL || "https://stage.backend.fsektionen.se";
 export const MAX_DOC_FILE_SIZE_MB = 25;
+
+export const MISSION_CATEGORY_ENUM = {
+	BARBIEDANCE: "Barbiedans",
+	MENTOR: "Fadder",
+	CREATIVE: "Kreativ",
+	TRIP: "Resa",
+	GAME: "Spel",
+	COMPETITION: "Tävling",
+	MOOSE: "Älg",
+} as const;
+
+export type MissionCategory =
+	(typeof MISSION_CATEGORY_ENUM)[keyof typeof MISSION_CATEGORY_ENUM];
