d274af9f1cd2ee0e2b6ce66e6a6e351c5847679c
Hannes Blåman <hannes@blaman.com>
2026-08-21 09:25:55 +0000

    Added daily/bonus level filtering

--- src/app/admin/enclose-moose/page.tsx
+++ src/app/admin/enclose-moose/page.tsx
@@ -11,6 +11,7 @@ import AdminPage from "@/widgets/AdminPage";
 import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import {
 	type ColumnDef,
+	type ColumnFiltersState,
 	createColumnHelper,
 	type Row,
 } from "@tanstack/react-table";
@@ -18,12 +19,25 @@ import EncloseMooseLevelEditForm from "./EncloseMooseLevelEditForm";
 import EncloseMooseLevelForm from "./EncloseMooseLevelForm";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { SelectFromOptions } from "@/widgets/SelectFromOptions";
+import { useState } from "react";
 
 export default function EncloseMoose() {
 	const { t } = useTranslation("admin");
 	const router = useRouter();
 
 	const columnHelper = createColumnHelper<EncloseMooseLevelRead>();
+
+	const dayIndexFilter = (row: any, columnId: string, filterValue: string) => {
+		const value = row.getValue(columnId);
+		switch (filterValue) {
+			case "daily":
+				return value !== null;
+			case "bonus":
+				return value === null;
+		}
+		return true;
+	};
 
 	const columns: ColumnDef<EncloseMooseLevelRead, any>[] = [
 		columnHelper.accessor("name_sv", {
@@ -46,6 +60,7 @@ export default function EncloseMoose() {
 						{t("enclose_moose.day_index_missing")}
 					</span>
 				),
+			filterFn: dayIndexFilter,
 		}),
 		// {
 		// 	id: "view",
@@ -64,6 +79,9 @@ export default function EncloseMoose() {
 		// },
 	];
 
+	const [filterValue, setFilterValue] = useState("all");
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
 	return (
 		<AdminPage
 			title={t("enclose_moose.page_title")}
@@ -74,7 +92,30 @@ export default function EncloseMoose() {
 			})}
 			columns={columns}
 			editComponent={EncloseMooseLevelEditForm}
-			headerButtons={<EncloseMooseLevelForm />}
+			headerButtons={
+				<>
+					<SelectFromOptions
+						className="w-auto"
+						options={[
+							{ value: "all", label: t("enclose_moose.view_all_levels") },
+							{ value: "daily", label: t("enclose_moose.view_daily_levels") },
+							{ value: "bonus", label: t("enclose_moose.view_bonus_levels") },
+						]}
+						value={filterValue}
+						onChange={(newFilter) => {
+							setColumnFilters(() =>
+								newFilter === "all"
+									? []
+									: [{ id: "day_index", value: newFilter }],
+							);
+							setFilterValue(newFilter);
+						}}
+					/>
+					<EncloseMooseLevelForm />
+				</>
+			}
+			columnFilters={columnFilters}
+			onColumnFiltersChange={setColumnFilters}
 		/>
 	);
 }
